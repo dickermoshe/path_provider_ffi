7876fe89e3d6d09223f8d614d7b755d59745b6e0
Moshe Dicker <dickermoshe@gmail.com>
2025-04-22 13:25:10 -0400

    try1

---
+++ .github/scripts/android-integration-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd example && {
+    xvfb-run flutter test ./integration_test/original_package_test.dar;
+    xvfb-run flutter test ./integration_test/path_provider_android_test.dart;
+}
